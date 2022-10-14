46fbd84324b84521211c02aca6ca045622d00ecc
dio222222 <dennisnklp@gmail.com>
2022-10-14 13:00:28 +0300

    Fetch "coin/markets" endpoint on the Back-End

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,41 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+// Call CoinGecko API on the Back-End 
+export async function getServerSideProps() {
+
+    let results
+    
+    try {
+        // Fetch Data
+        const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+        const data = await response.json()
+
+        // Destructuring Data to only pass relevant parameters to the Front-End
+        results = data.map((coin) => ({
+            name: coin.name,
+            symbol: coin.symbol,
+            image_url: coin.image,
+            current_price: coin.current_price,
+            high_24h: coin.high_24h,
+            low_24h: coin.low_24h,
+            price_change_24h: coin.price_change_24h
+        }))
+
+        console.log(results)
+
+    } catch (error) {
+        // If there were any Errors, return an empty Object
+        results = {}
+        console.log(error)
+    }
+
+    return {
+        props: { "data": { results } } 
+    }
+
+}
+
 export default function Home() {
   return (
     <>

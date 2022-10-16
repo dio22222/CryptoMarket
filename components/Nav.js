8197b7374c85c1b1f1b2000a880eebd3981fa858
dio222222 <dennisnklp@gmail.com>
2022-10-17 00:54:54 +0300

    Changed "class" to "className" in Navbar Component

--- components/Nav.js
+++ components/Nav.js
@@ -1,18 +1,18 @@
 export default function Nav() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand  text-primary" href="/">Crypto Market</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand  text-primary" href="/">Crypto Market</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                <div className="collapse navbar-collapse" id="navbarText">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="/">Home</a>
                     </li>
                 </ul>
-                <span class="navbar-text">
+                <span className="navbar-text">
                     The place that keeps you up to date!
                 </span>
                 </div>
